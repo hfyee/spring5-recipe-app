1b418a8558c965b524f7f90f35d088fed8bc0ac5
hfyee <hoongfai.yee@gmail.com>
2023-10-16 18:11:29 +0800

    Initialized DB with tables Category and UnitOfMeasure.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO category (id, description) VALUES (1, 'American');
+INSERT INTO category (id, description) VALUES (2, 'Italian');
+INSERT INTO category (id, description) VALUES (3, 'Mexican');
+INSERT INTO category (id, description) VALUES (4, 'Fast Food');
+INSERT INTO unit_of_measure (id, description) VALUES (1, 'Teaspoon');
+INSERT INTO unit_of_measure (id, description) VALUES (2, 'Tablespoon');
+INSERT INTO unit_of_measure (id, description) VALUES (3, 'Cup');
+INSERT INTO unit_of_measure (id, description) VALUES (4, 'Pinch');
+INSERT INTO unit_of_measure (id, description) VALUES (5, 'Ounce');
